5e4bbb42573bcaf444c9be56215c4223be4035ff
Aya0wind <120035959@qq.com>
2021-06-22 16:35:41 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,188 @@
-#include "utility.hpp"
-
+#include <array>
+#include <bit>
+#include <cstddef>
+#include <limits>
 #include <map>
-
 #include <stack>
-
+#include <string>
 #include <string_view>
-
 #include <unordered_map>
-
+#include <utility>
 #include <vector>
 
+#include "formatters.hpp"
+#include "utility.hpp"
 
 using namespace tools;
 using std::string;
+using std::unordered_map;
 using std::vector;
+// class Solution {
+// public:
+//     class myTree {
+//     private:
+//         void insertLetterOrPointEdge(char c, myTree* node) {
+//             node->edge[ c ] = new myTree{};
+//             edge[ c ]->father = node;
+//             node = edge[ c ];
+//         }
+//         void insertLetterStarEdge(char c, myTree* node) {
+//             node->edge[ c ] = node;
+//         }
+//         void insertPointStarEdge(char c, myTree* node) {
+//             node->edge[ '.' ] = node;
+//             node->edge[ c ] = new myTree{};
+//             edge[ c ]->father = node;
+//             edge[ c ]->edge[ c ] = edge[ c ]->father;
+//             node = edge[ c ];
+//         }
 
-constexpr std::array<int,101> absolute_square(){
-    std::array<int,101> array{};
-    for(int i=0;i<101;++i){
-        array[i]=i*i;
-    }
-    return array;
-}
-class Solution {
-    static constexpr std::array<int,101> table = absolute_square();
-    static int getMin(const vector<int>& dp,int n){
-        int sqrt=static_cast<int>(std::sqrt(n));
-        int min=INT_MAX;
-        for(int diff=sqrt;diff>0;--diff){
-            min=std::min(min,dp[n-table[diff]]);
-        }
-        return min;
-    }
-public:
-    int numSquares(int n) {
-        auto dp=vector<int>(n+1);
-        dp[0]=0;
-        for(int i=1;i<=n;++i){
-            dp[i]=getMin(dp,i)+1;
-        }
-        return dp[n];
+//     public:
+//         bool state = false;
+//         myTree* father;
+//         unordered_map<char, myTree*> edge;
+
+//         void getPattern(string pattern) {
+//             int n = pattern.size();
+//             myTree* node = this;
+//             for (int i = 0; i < n - 1;) {
+//                 if (pattern[ i ] == '.' && pattern[ i + 1 ] == '*') {
+//                     if (i + 2 < n && pattern[ i + 2 ] != '.') {
+//                         insertPointStarEdge(pattern[ i + 2 ], node);
+//                         i += 3;
+//                     } else {
+//                     }
+//                 } else if (pattern[ i + 1 ] == '*') {
+//                     insertLetterStarEdge(pattern[ i ], node);
+//                     std::cout<<node<<"    "<<node->edge[ 'a' ];
+//                     i += 2;
+//                 } else {
+//                     insertLetterOrPointEdge(pattern[ i ], node);
+//                     i++;
+//                 }
+//             }
+//             if (pattern[ n - 1 ] != '.' && node->edge.find(pattern[ n - 1 ]) == edge.end()) {
+//                 node->edge[ pattern[ n - 1 ] ] = new myTree{};
+//                 node = edge[ pattern[ n - 1 ] ];
+//             }
+//             node->state = true;
+//             std::cout << node << "    " << node->edge[ 'a' ];
+//         }
+//         bool isMatch(string s) {
+//             myTree* node = this;
+//             int n = s.size();
+//             for (int i = 0; i < n; i++) {
+//                 if (node->edge.count(s[ i ])) {
+//                     node = node->edge[ s[ i ] ];
+//                 } else if (node->edge.count('.')) {
+//                     node = node->edge[ '.' ];
+//                 } else {
+//                     return false;
+//                 }
+//             }
+//             return node->state;
+//         }
+//     };
+//     bool isMatch(string s, string p) {
+//         myTree map{};
+//         map.getPattern(p);
+//         return map.isMatch(s);
+//     }
+// };
+
+struct GraphNode {
+    constexpr static char Any = std::numeric_limits<char>::max();
+    constexpr static char End = std::numeric_limits<char>::min();
+    unordered_map<char, GraphNode*> edge;
+    auto haveEdge(char ch) {
+        auto it = edge.find(ch);
+        auto itAny = edge.find(GraphNode::Any);
+        return std::make_pair(it != edge.end() ? it->second : nullptr, itAny != edge.end() ? itAny->second : nullptr);
     }
 };
 
+class NFAGraph {
+    GraphNode* startNode;
 
-int main() {
-    tools::println("out:{}",Solution{}.numSquares(13));
+    static void addRepeatNode(GraphNode*& thisNode, char ch) {
+        auto newNode = new GraphNode;
+        newNode->edge[ ch ] = newNode;
+        if (ch == '.') {
+            thisNode->edge[ GraphNode::Any ] = newNode;
+        } else {
+            thisNode->edge[ ch ] = newNode;
+        }
+        thisNode = newNode;
+    }
 
+    static void addSingleNode(GraphNode*& thisNode, char ch) {
+        auto newNode = new GraphNode;
+        if (ch == '.') {
+            thisNode->edge[ GraphNode::Any ] = newNode;
+        } else {
+            thisNode->edge[ ch ] = newNode;
+        }
+        thisNode = newNode;
+    }
 
+    bool dfs(GraphNode* startNode, auto pChar, auto pEnd) {
+        if (pChar == pEnd) {
+            if (startNode->edge.find(GraphNode::End) != startNode->edge.end()) {
+                return true;
+            }
+            return false;
+        }
+        auto [ loopEdge, nextEdge ] = startNode->haveEdge(*pChar);
+        if (loopEdge == nullptr && nextEdge == nullptr) {
+            return false;
+        }
+        return dfs(loopEdge, pChar + 1, pEnd) || dfs(nextEdge, pChar + 1, pEnd);
+    }
 
+public:
+    NFAGraph(const std::string& pattern)
+        : startNode(new GraphNode) {
+        GraphNode* thisNode = startNode;
+        for (int i = 0; i < pattern.size(); ++i) {
+            if (i + 1 < pattern.size() && pattern[ i + 1 ] == '*') {
+                addRepeatNode(thisNode, pattern[ i ]);
+                ++i;
+            } else {
+                addSingleNode(thisNode, pattern[ i ]);
+            }
+        }
+        thisNode->edge[ GraphNode::End ] = nullptr;
+    }
 
-
-
-
-
-//    auto v0 = vector{1, 2, 3, 4, 5, 6};
-//
-//    auto v1 = vector{vector{123, 12, 4, 3}, vector{123, 12, 4, 3}};
-//
-//    auto l0 = LinkedList{1, 2, 3, 4, 5, 6};
-//
-//    auto b0 = BinaryTree{"[1,2,3,4,5]"};
-//
-//    auto s0 = std::string("12345-std string");
-//
-//    auto cc0 = "12345-cstring";
-//
-//    auto hmap0 = std::unordered_map<int, int>{};
-//    hmap0[ 1 ] = 1;
-//    hmap0[ 2 ] = 2;
-//
-//    auto hmap1 = std::unordered_map<int, std::string>{};
-//    hmap1[ 1 ] = "123";
-//    hmap1[ 2 ] = "123";
-//
-//    auto map0 = std::map<int, int>{};
-//    map0[ 1 ] = 1;
-//    map0[ 2 ] = 2;
-//
-//    auto map1 = std::map<int, std::string>{};
-//    map1[ 1 ] = "123";
-//    map1[ 2 ] = "123";
-//
-//    auto tuple = std::make_tuple(1, 1.23345, "abcde");
-//
-//    auto pair = std::make_pair(1.23345, "abcde");
-//
-//    tools::println("vector1->{}\nvector2->{}\nLinkedList->{}\nBinaryTree->{}", v0, v1, l0, b0);
-//
-//    tools::println("std-string->{}\nc-string->{}", s0, cc0);
-//
-//    tools::println("hashmap 0->{}\nhashmap 1->{}", hmap0, hmap1);
-//
-//    tools::println("map 0->{}\nmap 1->{}", map0, map1);
-//    tools::println("tuple->{}\npair->{}\nfloat number->{:.4f}", tuple, pair, 1.23154687);
-//
-//    LinkedList l1{1, 2, 6, 3, 4, 5, 6};
-//
-//    tools::println("input->{}", l1);
-//
-//    tools::println("vector:{}", v0);
-
+    bool match(const std::string& str) {
+        return dfs(this->startNode, str.begin(), str.end());
+    }
+};
+static std::array<std::vector<std::string>, 10> generateTable() {
+    auto table = std::array<std::vector<std::string>, 10>{};
+    auto compute = [](unsigned int turnedOn) {
+        vector<std::string> ans;
+        for (unsigned int h = 0; h < 12; ++h) {
+            for (unsigned int m = 0; m < 60; ++m) {
+                if (std::popcount(h) + std::popcount(m) == turnedOn) {
+                    ans.push_back(std::to_string(h) + ":" + (m < 10 ? "0" : "") + std::to_string(m));
+                }
+            }
+        }
+        return ans;
+    };
+    for (unsigned int i = 0; i < table.size(); ++i) {
+        table[ i ] = compute(i);
+    }
+    return table;
 }
-
+auto table=generateTable();
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        return table[turnedOn];
+    }
+};
+int main() {
+     tools::println("{}", Solution{}.readBinaryWatch(10)));
+}
